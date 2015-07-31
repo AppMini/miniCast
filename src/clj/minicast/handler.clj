07f27405a550e5485162b520c5224ef06bd68927
Chris McCormick <chris@mccormick.cx>
2015-07-31 10:00:53 +0800

    Nit - reshaping forms.

--- src/clj/minicast/handler.clj
+++ src/clj/minicast/handler.clj
@@ -16,9 +16,7 @@
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
      (include-css "css/loaders.css/loaders.min.css")
-     [:link
-       {:href "http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-        :rel "stylesheet" :type "text/css"}] 
+     ;[:link {:href "http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" :rel "stylesheet" :type "text/css"}] 
      (include-css "css/Font-Awesome/css/font-awesome.min.css")
      (include-css (if (env :dev) "css/site.css" "css/site.min.css"))
      ]
